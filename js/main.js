36cf1a9dee6cb31ebd5ed728faa536999e077747
Patrick Estabrook <patrick.m.estabrook@gmail.com>
2013-02-14 12:28:48 -0500

    Replaced boop sounds with drum samples, added third track

--- js/main.js
+++ js/main.js
@@ -4,23 +4,19 @@ $(document).ready(function() {
   var currentStep = 1;
   var time;
   var NUM_OF_STEPS = 16;
-  var NUM_OF_TRACKS = 2;
+  var NUM_OF_TRACKS = 3;
   var SIXTEENTH_NOTE_TIME = 150; // ms
 
   // populate the `sounds` object
   sounds = {};
-  sounds.num1 = new Sound("audio/num1.mp3", 0.3);
-  sounds.num2 = new Sound("audio/num2.mp3", 0.3);
-
-  // these are not yet used
-  sounds.num3 = new Sound("audio/num3.mp3", 0.3);
-  sounds.num4 = new Sound("audio/num4.mp3", 0.3);
-  sounds.num5 = new Sound("audio/num5.mp3", 0.3);
+  sounds.kick = new Sound("audio/kick.wav", 0.3);
+  sounds.snare = new Sound("audio/snare.wav", 0.3);
+  sounds.hihat = new Sound("audio/hihat.wav", 0.3);
 
   // insert the html elements of the sequencer tracks and buttons
   for ( i = 1; i <= NUM_OF_TRACKS; i++) {
 
-    $('.sequencer').append('<div class="sequencer-track off">');
+    //$('.sequencer').append('<div class="sequencer-track">');
 
       for ( j = 1; j <= NUM_OF_STEPS; j++) {
 
@@ -31,7 +27,7 @@ $(document).ready(function() {
           $('.sequencer').append('<div class="sequencer-button off"></div>');
         }
       }
-    $('.sequencer').append('</div>');
+    //$('.sequencer').append('</div>');
   }
 
   // populate the `buttons` object
@@ -56,10 +52,13 @@ $(document).ready(function() {
     time = audioContext.currentTime;
 
     if ( buttons[1][currentStep].hasClass('on') ) {
-      sounds[ "num1" ].play(time + 0.1);
+      sounds[ "hihat" ].play(time + 0.1);
     }
     if ( buttons[2][currentStep].hasClass('on') ) {
-      sounds[ "num2" ].play(time + 0.1);
+      sounds[ "snare" ].play(time + 0.1);
+    }
+    if ( buttons[3][currentStep].hasClass('on') ) {
+      sounds[ "kick" ].play(time + 0.1);
     }
 
     console.log(currentStep);
@@ -103,13 +102,16 @@ $(document).ready(function() {
     var $this = $(this);
     
     if ($this.hasClass('off')) {
-      $this.addClass('on');
-      $this.removeClass('off');
+      $this.addClass('on').removeClass('off');
     }
     else {
-      $this.addClass('off');
-      $this.removeClass('on');
+      $this.addClass('off').removeClass('on');
     }
+  });
+
+  // handles clear button event
+  $('.clear').click(function() {
+    $('.sequencer-button').removeClass('on').addClass('off');
   });
 
 
